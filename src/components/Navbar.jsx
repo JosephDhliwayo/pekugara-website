8b8f75eea4ff260fc4a7d1e81526568d6f9157d9
joedeey27 <mrdhliwayojoseph@gmail.com>
2026-06-10 03:42:03 -0400

    Revert "Replace docs icon with animated graduation cap SVG"
    This reverts commit 80b2741011378c2ae9ec119b7e480115dc419d0d.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,4 @@
 import { useState } from 'react'
-import DocsAnimIcon from './DocsAnimIcon'
 
 const APPLE_STORE_URL = '#'
 const PLAY_STORE_URL  = '#'
@@ -32,7 +31,7 @@ export default function Navbar({ onStoreClick }) {
           {[['#home','Home'],['#features','Features'],['#how','How It Works'],['#about','About'],['#contact','Contact']].map(([href,label]) => (
             <a key={href} href={href} className="nav-link">{label}</a>
           ))}
-          <a href="/docs" className="nav-link nav-link-docs"><DocsAnimIcon size={18} /> User Docs</a>
+          <a href="/docs" className="nav-link nav-link-docs">📚 User Docs</a>
         </div>
 
         <div className="nav-btns">
@@ -56,7 +55,7 @@ export default function Navbar({ onStoreClick }) {
           {[['#home','Home'],['#features','Features'],['#how','How It Works'],['#about','About'],['#contact','Contact']].map(([href,label]) => (
             <a key={href} href={href} className="mobile-link" onClick={() => setOpen(false)}>{label}</a>
           ))}
-          <a href="/docs" className="mobile-link mobile-link-docs" onClick={() => setOpen(false)}><DocsAnimIcon size={20} /> User Documentation</a>
+          <a href="/docs" className="mobile-link mobile-link-docs" onClick={() => setOpen(false)}>📚 User Documentation</a>
           <div className="mobile-store-btns">
             <button className="mobile-btn-ios" onClick={() => { setOpen(false); onStoreClick('ios') }}>
               <AppleIcon /> Download on the App Store
